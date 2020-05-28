163b0d8b6d6928e8d50e620b5a71ad222b1f97c7
Elias Tandel <elias.tandel@stone.com.br>
2020-05-27 22:07:35 -0300

    Implement write_response()

--- src/handler.rs
+++ src/handler.rs
@@ -1,4 +1,5 @@
-use std::io::BufRead;
+use std::error::Error;
+use std::io::{BufRead, Write};
 
 use http::method::Method;
 use http::request::Request;
@@ -27,9 +28,22 @@ fn get_response(reader: &mut dyn BufRead) -> Response<()> {
     }
 }
 
+fn write_response(resp: &Response<()>, writer: &mut dyn Write) -> Result<(), Box<dyn Error>> {
+    write!(
+        writer,
+        "{} {}\r\n",
+        resp.status().as_str(),
+        resp.status().canonical_reason().unwrap_or("UNKNOWN")
+    )?;
+    write!(writer, "{}\r\n", "Connection: close")?;
+    write!(writer, "{}\r\n", "Content-type: text/plain")?;
+    write!(writer, "{}\r\n", "Content-length: 0")?;
+    write!(writer, "\r\n")?;
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
-    use std::error::Error;
     use std::io::BufReader;
 
     use super::*;
@@ -51,10 +65,43 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
     fn get_response_with_parse_error() {
         let raw_request = "".as_bytes();
         let got = get_response(&mut BufReader::new(raw_request));
-        assert_eq!(got.status(), 404);
+        assert_eq!(got.status(), 400);
+    }
+
+    #[test]
+    fn write_response_ok() -> Result<(), Box<dyn Error>> {
+        let resp = Response::builder().status(200).body(())?;
+        let mut writer = Vec::new();
+        write_response(&resp, &mut writer)?;
+        let got = String::from_utf8(writer)?;
+        assert_eq!(
+            &got,
+            "200 OK\r\nConnection: close\r\nContent-type: text/plain\r\nContent-length: 0\r\n\r\n"
+        );
+        Ok(())
+    }
+
+    use std::io;
+    struct ErrWriter();
+    impl Write for ErrWriter {
+        fn write(&mut self, _buf: &[u8]) -> io::Result<usize> {
+            Err(io::Error::new(io::ErrorKind::Other, "always fails"))
+        }
+
+        fn flush(&mut self) -> io::Result<()> {
+            Err(io::Error::new(io::ErrorKind::Other, "always fails"))
+        }
+    }
+
+    #[test]
+    fn write_response_err() -> Result<(), Box<dyn Error>> {
+        let resp = Response::builder().status(200).body(())?;
+        let mut writer = ErrWriter();
+        let got = write_response(&resp, &mut writer);
+        assert!(got.is_err(), "Error expected");
+        Ok(())
     }
 }
